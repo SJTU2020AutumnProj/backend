7e98c8e940beb6f373dec03adfe79b3ae240d10d
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-11-18 22:38:46 +0800

    fix：修复了不能改user表的ID的bug

--- service/user/repository/user.go
+++ service/user/repository/user.go
@@ -53,6 +53,7 @@ func (repo *UserRepositoryImpl) UpdateUser(ctx context.Context, user User) error
 	tmp.UserName = user.UserName
 	tmp.Password = user.Password
 	tmp.School = user.School
+	tmp.ID = user.ID
 	tmp.Phone = user.Phone
 	tmp.Email = user.Email
 	if err = repo.DB.Save(tmp).Error; nil != err {
