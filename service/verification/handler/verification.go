03c50085d1f3b0ece3d38e1e26bf3e5ca0ebf5ef
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-12-15 23:27:15 +0800

    verification first commit, still has bug

---
+++ service/verification/handler/verification.go
@@ -0,0 +1,91 @@
+package handler
+
+import (
+	pb "boxin/service/verification/proto/verification"
+	repo "boxin/service/verification/repository"
+	"context"
+	"fmt"
+	"math/rand"
+	"net/smtp"
+	"strconv"
+	"time"
+	"log"
+)
+
+/*
+VerificationHandler handler
+*/
+type VerificationHandler struct {
+	VerificationRepository repo.VerificationRepository
+}
+
+/*
+Configuration of smtp service
+*/
+const (
+	SMTPMailHost = "smtp.163.com"
+	SMTPMailPort = "25"
+	SMTPMailUser = "sjtuboxin@163.com"
+	SMTPMailPwd  = "RBMJNDBOVZPEKFJQ"
+	ExpireTime = "300"
+)
+
+/*
+SendCodeEmail send verification code to the given email address
+*/
+func (v *VerificationHandler) SendCodeEmail(ctx context.Context, in *pb.SendCodeEmailParam, out *pb.SendCodeEmailResponse) error {
+	auth := smtp.PlainAuth(in.Username, SMTPMailUser, SMTPMailPwd, SMTPMailHost)
+	nickname := "Boxin"
+	contentType := "Content-Type: text/html; charset=UTF-8"
+	randomNumber := strconv.Itoa(int(rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000)))
+	body := "Dear " + in.Username + ", your verification code is " + "<b>" + randomNumber + "</b>"
+	msg := []byte("To: " + in.Email + "\r\nFrom: " + nickname + "<" + SMTPMailUser + ">\r\nSubject: " + "Your verification code" +
+		"\r\n" + contentType + "\r\n\r\n" + body)
+	err := smtp.SendMail(fmt.Sprintf("%s:%s", SMTPMailHost, SMTPMailPort), auth, SMTPMailUser, []string{in.Email}, msg)
+	if nil != err {
+		log.Println("VerificationHandler SendCodeEmail error ", err)
+		out = &pb.SendCodeEmailResponse{
+			Status: -1,
+			Message: "SMTP send email error",
+		}
+		return err
+	}
+	setErr := v.VerificationRepository.Set(ctx, in.Email, randomNumber, ExpireTime)
+	if nil != setErr {
+		log.Println("VerificationHandler redis error ", setErr)
+		out = &pb.SendCodeEmailResponse{
+			Status: -1,
+			Message: "Redis set key value error",
+		}
+		return setErr
+	}
+	return nil
+}
+
+/*
+VerifyCodeEmail verify the code
+*/
+func (v *VerificationHandler) VerifyCodeEmail(ctx context.Context, in *pb.VerifyCodeEmailParam, out *pb.VerifyCodeEmailResponse) error {
+	actualCode, err := v.VerificationRepository.Get(ctx, in.Email)
+	if nil != err {
+		log.Println("VerificationHandler reids error ", err)
+		out = &pb.VerifyCodeEmailResponse{
+			Status: -1,
+			Message: "Redis error: " + err.Error(),
+		}
+		return err
+	}
+	if !(actualCode == in.Code) {
+		log.Println("VerificationHandler verification code mismatch, given ", in.Code, "actual ", actualCode)
+		out = &pb.VerifyCodeEmailResponse{
+			Status: -1,
+			Message: "Verification code mismatch",
+		}
+		return nil
+	}
+	out = &pb.VerifyCodeEmailResponse{
+		Status: 0,
+		Message: "Verified",
+	}
+	return nil
+}
---
+++ service/verification_test/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	verification "boxin/service/verification/proto/verification"
+	"context"
+	"log"
+
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+)
+
+const (
+	ServiceName = "go.micro.client.verification"
+	EtcdAddr    = "localhost:2379"
+)
+
+func main() {
+	server := micro.NewService(
+		micro.Name(ServiceName),
+		micro.Registry(etcd.NewRegistry(
+			registry.Addrs(EtcdAddr),
+		)),
+	)
+	server.Init()
+	verificationService := verification.NewVerificationService("go.micro.service.verification", server.Client())
+	// sendCode(verificationService, "chengke", "chengke3@163.com")
+	verifyCode(verificationService, "chengke", "chengke3@163.com", "20221")
+}
+
+func sendCode(verificationService verification.VerificationService, username string, email string) {
+	resp, err := verificationService.SendCodeEmail(
+		context.Background(),
+		&verification.SendCodeEmailParam{
+			Email: email,
+			Username: username,
+		},
+	)
+	if nil != err {
+		log.Println("sendCode error, ", err)
+		return
+	}
+	if resp.Status != 0 {
+		log.Println("sendCode error, ", resp.Message)
+		return
+	}
+	log.Println("sendCode success ", resp.Message)
+}
+
+func verifyCode(verificationService verification.VerificationService, username string, email string, code string) {
+	resp, err := verificationService.VerifyCodeEmail(
+		context.Background(),
+		&verification.VerifyCodeEmailParam{
+			Email: email,
+			Username: username,
+			Code: code,
+		},
+	)
+	if nil != err {
+		log.Println("verifyCode error, ", err)
+		return
+	}
+	if resp.Status != 0 {
+		log.Println("verifyCode error, ", resp.Message)
+		return
+	}
+	log.Println("verifyCode success ", resp.Message)
+}
