1fd59c96bba43bdf574daecf4851f34bf71015ae
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2021-01-03 16:46:52 +0800

    modify message content

--- service/message/subscriber/message.go
+++ service/message/subscriber/message.go
@@ -2,8 +2,8 @@ package subscriber
 
 import (
 	homework "boxin/service/homework/proto/homework"
-	repo "boxin/service/message/repository"
 	mongo "boxin/service/message/mongoDB"
+	repo "boxin/service/message/repository"
 	"context"
 	"log"
 	"time"
@@ -12,7 +12,7 @@ import (
 // MessageSub struct
 type MessageSub struct {
 	MessageRepository repo.MessageRepository
-	MongoDB mongo.MessageMongo
+	MongoDB           mongo.MessageMongo
 }
 
 // Assigned add an assigned homework message
@@ -24,21 +24,21 @@ func (sub *MessageSub) Assigned(ctx context.Context, homework *homework.Assigned
 		repo.Message{
 			MessageTime: now,
 			MessageType: 0,
-			CourseID: homework.CourseID,
-			Title: homework.Title + " 已经发布",
-			State: 0,
+			CourseID:    homework.CourseID,
+			Title:       homework.Title + " 已经发布",
+			State:       0,
 		},
 	)
 	if nil != err {
 		log.Println("Subscriber Assigned add new message in Mysql error ", err)
 		return err
 	}
-	content := homework.Title + " 已经发布，" + "截止日期为 " + time.Unix(homework.EndTime, 0).Format("2006-01-02 15:04:05")
+	content := homework.Title + " 已经发布，" + "截止时间为 " + time.Unix(homework.EndTime, 0).Format("2006-01-02 15:04:05")
 	err = sub.MongoDB.AddMessage(
 		ctx,
 		mongo.Message{
 			MessageID: message.MessageID,
-			Content: content,
+			Content:   content,
 		},
 	)
 	if nil != err {
