2a3b43d726f3283b7ebbd3bb9564084ad1cf5bef
Liu Shenheng <55398344+shenhliu@users.noreply.github.com>
2021-01-05 23:28:33 +0800

    fix updateAnswer bug

--- service/answer/repository/answer.go
+++ service/answer/repository/answer.go
@@ -11,18 +11,17 @@ import (
 )
 
 type Answer struct {
-	AnswerID int32 `gorm:"auto_increment;column:answer_id;primary_key:true;unique;index:"`
+	AnswerID   int32     `gorm:"auto_increment;column:answer_id;primary_key:true;unique;index:"`
 	CommitTime time.Time `gorm:"not null;column:commit_time"`
 }
 
 type UserHomework struct {
 	HomeworkID int32 `gorm:"column:homework_id;primary_key:true;index:"`
-	UserID int32 `gorm:"column:user_id;primary_key:true;index:"`
-	AnswerID int32 `gorm:"column:answer_id"`
-	CheckID int32 `gorm:"column:check_id"`
-	State int32 `gorm:"column:state"`
+	UserID     int32 `gorm:"column:user_id;primary_key:true;index:"`
+	AnswerID   int32 `gorm:"column:answer_id"`
+	CheckID    int32 `gorm:"column:check_id"`
+	State      int32 `gorm:"column:state"`
 }
-
 
 func (Answer) TableName() string {
 	return "answer"
@@ -33,35 +32,35 @@ func (UserHomework) TableName() string {
 }
 
 type AnswerRepository interface {
-	AddAnswer(ctx context.Context, answer Answer)(Answer, error)
+	AddAnswer(ctx context.Context, answer Answer) (Answer, error)
 	DeleteAnswer(ctx context.Context, answerID int32) error
 	UpdateAnswer(ctx context.Context, answer Answer) error
 	SearchAnswer(ctx context.Context, answerID int32) (Answer, error)
-	SearchAnswerByHomeworkID(ctx context.Context, homeworkID int32) ([]*Answer,error)
-	SearchAnswerByUserID(ctx context.Context, userID int32) ([]*Answer,error)
-	PostAnswerByStudent(ctx context.Context, userID int32,homeworkID int32, state int32,answer Answer) (Answer,error)
-	PostAnswerByTeacher(ctx context.Context, userID int32,homeworkID int32, answer Answer) (Answer,error)
+	SearchAnswerByHomeworkID(ctx context.Context, homeworkID int32) ([]*Answer, error)
+	SearchAnswerByUserID(ctx context.Context, userID int32) ([]*Answer, error)
+	PostAnswerByStudent(ctx context.Context, userID int32, homeworkID int32, state int32, answer Answer) (Answer, error)
+	PostAnswerByTeacher(ctx context.Context, userID int32, homeworkID int32, answer Answer) (Answer, error)
 }
 
-type AnswerRepositoryImpl struct{
+type AnswerRepositoryImpl struct {
 	DB *gorm.DB
 }
 
-func(repo *AnswerRepositoryImpl) AddAnswer(ctx context.Context,answer Answer) (Answer, error){
-	if err := repo.DB.Create(&answer).Error;nil != err {
-		return Answer{},err
+func (repo *AnswerRepositoryImpl) AddAnswer(ctx context.Context, answer Answer) (Answer, error) {
+	if err := repo.DB.Create(&answer).Error; nil != err {
+		return Answer{}, err
 	}
-	return answer,nil
+	return answer, nil
 }
 
-func(repo *AnswerRepositoryImpl) DeleteAnswer(ctx context.Context,answerID int32) error{
+func (repo *AnswerRepositoryImpl) DeleteAnswer(ctx context.Context, answerID int32) error {
 	if err := repo.DB.Delete(&Answer{}, answerID).Error; nil != err {
 		return err
 	}
 	return nil
 }
 
-func(repo *AnswerRepositoryImpl) SearchAnswer(ctx context.Context,answerID int32) (Answer, error){
+func (repo *AnswerRepositoryImpl) SearchAnswer(ctx context.Context, answerID int32) (Answer, error) {
 	var answer Answer
 	result := repo.DB.First(&answer, answerID)
 	if nil != result.Error {
@@ -70,16 +69,16 @@ func(repo *AnswerRepositoryImpl) SearchAnswer(ctx context.Context,answerID int32
 	return answer, result.Error
 }
 
-func(repo *AnswerRepositoryImpl) UpdateAnswer(ctx context.Context,answer Answer) error{
+func (repo *AnswerRepositoryImpl) UpdateAnswer(ctx context.Context, answer Answer) error {
 	tmp, err := repo.SearchAnswer(ctx, answer.AnswerID)
 	tmp.CommitTime = answer.CommitTime
-	if err = repo.DB.Save(tmp).Error; nil != err {
+	if err = repo.DB.Model(&tmp).Updates(tmp).Error; nil != err {
 		return err
 	}
 	return nil
 }
 
-func (repo *AnswerRepositoryImpl) SearchAnswerByUserID(ctx context.Context,userID int32) ([]*Answer,error){
+func (repo *AnswerRepositoryImpl) SearchAnswerByUserID(ctx context.Context, userID int32) ([]*Answer, error) {
 	var user_homework []*Answer
 
 	result := repo.DB.Table("user_homework").Where("user_id = ?", userID)
@@ -101,7 +100,7 @@ func (repo *AnswerRepositoryImpl) SearchAnswerByUserID(ctx context.Context,userI
 	return answers, nil
 }
 
-func (repo *AnswerRepositoryImpl) SearchAnswerByHomeworkID(ctx context.Context, homeworkID int32) ([]*Answer, error){
+func (repo *AnswerRepositoryImpl) SearchAnswerByHomeworkID(ctx context.Context, homeworkID int32) ([]*Answer, error) {
 	var user_homework []*Answer
 
 	result := repo.DB.Table("user_homework").Where("homework_id = ?", homeworkID)
@@ -123,40 +122,40 @@ func (repo *AnswerRepositoryImpl) SearchAnswerByHomeworkID(ctx context.Context, 
 	return answers, nil
 }
 
-func (repo *AnswerRepositoryImpl) PostAnswerByStudent(ctx context.Context,userID int32,homeworkID int32, state int32,answer Answer) (Answer, error){
-	if err := repo.DB.Create(&answer).Error;nil != err {
-		return Answer{},err
+func (repo *AnswerRepositoryImpl) PostAnswerByStudent(ctx context.Context, userID int32, homeworkID int32, state int32, answer Answer) (Answer, error) {
+	if err := repo.DB.Create(&answer).Error; nil != err {
+		return Answer{}, err
 	}
 
 	um := UserHomework{
-		HomeworkID:	homeworkID,
-		UserID:		userID,
-		AnswerID: 	answer.AnswerID,
-		CheckID: 	-1,
-		State:		state,
+		HomeworkID: homeworkID,
+		UserID:     userID,
+		AnswerID:   answer.AnswerID,
+		CheckID:    -1,
+		State:      state,
 	}
 
-	if err := repo.DB.Create(&um).Error;nil != err {
-		return Answer{},err
+	if err := repo.DB.Create(&um).Error; nil != err {
+		return Answer{}, err
 	}
-	return answer,nil
+	return answer, nil
 }
 
-func (repo *AnswerRepositoryImpl) PostAnswerByTeacher(ctx context.Context,userID int32,homeworkID int32,answer Answer) (Answer, error){
-	if err := repo.DB.Create(&answer).Error;nil != err {
-		return Answer{},err
+func (repo *AnswerRepositoryImpl) PostAnswerByTeacher(ctx context.Context, userID int32, homeworkID int32, answer Answer) (Answer, error) {
+	if err := repo.DB.Create(&answer).Error; nil != err {
+		return Answer{}, err
 	}
 
 	um := UserHomework{
-		HomeworkID:	homeworkID,
-		UserID:		userID,
-		AnswerID: 	answer.AnswerID,
-		CheckID: 	-1,
-		State:		-1,
+		HomeworkID: homeworkID,
+		UserID:     userID,
+		AnswerID:   answer.AnswerID,
+		CheckID:    -1,
+		State:      -1,
 	}
 
 	if err := repo.DB.Save(um).Error; nil != err {
-		return Answer{},err
+		return Answer{}, err
 	}
-	return answer,nil
+	return answer, nil
 }
