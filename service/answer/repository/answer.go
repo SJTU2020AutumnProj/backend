d2d9acb7974a9464ef77b4786f0d6bce969c8598
Liu Shenheng <55398344+shenhliu@users.noreply.github.com>
2021-01-08 03:24:07 +0800

    Add files via upload

--- service/answer/repository/answer.go
+++ service/answer/repository/answer.go
@@ -123,7 +123,7 @@ func (repo *AnswerRepositoryImpl) SearchAnswerByHomeworkID(ctx context.Context, 
 }
 
 func (repo *AnswerRepositoryImpl) PostAnswerByStudent(ctx context.Context, userID int32, homeworkID int32, state int32, answer Answer) (Answer, error) {
-	if err := repo.DB.Create(&answer).Error; nil != err {
+	if err := repo.DB.Table("answer").Model(&answer).Create(answer).Error; nil != err {
 		return Answer{}, err
 	}
 
@@ -135,14 +135,14 @@ func (repo *AnswerRepositoryImpl) PostAnswerByStudent(ctx context.Context, userI
 		State:      state,
 	}
 
-	if err := repo.DB.Update(&um).Error; nil != err {
+	if err := repo.DB.Table("user_homework").Model(&um).Updates(um).Error; nil != err {
 		return Answer{}, err
 	}
 	return answer, nil
 }
 
 func (repo *AnswerRepositoryImpl) PostAnswerByTeacher(ctx context.Context, userID int32, homeworkID int32, answer Answer) (Answer, error) {
-	if err := repo.DB.Create(&answer).Error; nil != err {
+	if err := repo.DB.Table("answer").Model(&answer).Create(answer).Error; nil != err {
 		return Answer{}, err
 	}
 	// um := UserHomework{
